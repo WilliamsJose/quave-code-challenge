a7f8faf0603484e55def4b75fabd558ad8aef11f
will <contatowilliamsj@gmail.com>
2024-01-30 19:17:34 -0300

    feat: data loaded on table and selector

--- ui/home/index.js
+++ ui/home/index.js
@@ -1,14 +1,10 @@
 import { Autocomplete, Divider, TextField } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BoxDataGrid } from '../component/datagrid/BoxDataGrid';
 import { People } from '../../people/people';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-
-const options = [
-  { label: 'test', id: 1 },
-  { label: 'best', id: 2 },
-];
+import { Communities } from '../../communities/communities';
 
 const peopleInEvent = 18;
 const groups = [
@@ -23,72 +19,99 @@ const peopleNotCheckedIn = 132;
 const joinGroupsStr = groups.map(group => `${group.name} (${group.count})`).join(', ');
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 60 },
+  { field: 'id', headerName: 'ID', width: 90 },
   {
     field: 'fullName',
     headerName: 'Name',
-    width: 140,
+    width: 160,
     editable: false,
   },
   {
     field: 'company',
     headerName: 'Company',
-    width: 140,
+    width: 160,
     editable: false,
   },
   {
     field: 'title',
     headerName: 'Title',
-    width: 140,
+    width: 160,
     editable: false,
   },
   {
     field: 'checkIn',
     headerName: 'Check-in',
     type: 'Date',
-    width: 140,
+    width: 160,
     editable: false,
   },
   {
     field: 'checkOut',
     headerName: 'Check-out',
     type: 'Date',
-    width: 140,
+    width: 160,
     editable: false,
   },
 ];
 
-const rows = [
-  { id: 1, fullName: 'Maria Carla', company: 'Green Group', title: 'UX/UI', checkIn: '30/21/24 11:15', checkOut: '30/21/24 13:45' },
-  { id: 2, fullName: 'Maria Carla', company: 'Green Group', title: 'UX/UI', checkIn: '30/21/24 12:15', checkOut: '30/21/24 13:45' },
-  { id: 3, fullName: 'Maria Carla', company: 'Green Group', title: 'UX/UI', checkIn: '30/21/24 13:15', checkOut: '30/21/24 13:45' },
-  { id: 4, fullName: 'Maria Carla', company: 'Green Group', title: 'UX/UI', checkIn: '30/21/24 14:15', checkOut: '30/21/24 16:45' },
-  { id: 5, fullName: 'Maria Carla', company: 'Green Group', title: 'UX/UI', checkIn: '30/21/24 14:15', checkOut: '30/21/24 17:45' },
-  { id: 6, fullName: 'Maria Carla', company: 'Green Group', title: 'UX/UI', checkIn: '30/21/24 14:15', checkOut: '30/21/24 16:45' },
-];
-
 export const Home = () => {
-  const newPerson = useTracker(() => {
+  const [options, setOptions] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  const communities = useTracker(() => {
+    const handle = Meteor.subscribe('communities');
+    const loading = !handle.ready();
+    const foundCommunities = Communities.find().fetch();
+    return {
+      loading,
+      foundCommunities,
+    };
+  });
+
+  const people = useTracker(() => {
     const handle = Meteor.subscribe('people');
     const loading = !handle.ready();
-    const person = People.findOne({ firstName: 'Carl' });
-    console.log(person);
+    const foundPeople = People.find().fetch();
     return {
-      person,
+      foundPeople,
       loading,
     };
   });
 
+  useEffect(() => {
+    if (!communities.loading) {
+      setOptions(prevOptions => (
+        communities.foundCommunities.reduce((acc, community) => (
+          [{ label: community.name, id: community.name }, ...acc]
+        ), prevOptions)
+      ));
+    }
+  }, [communities.loading]);
+
+  useEffect(() => {
+    if (!people.loading) {
+      setRows(prevOptions => (
+        people.foundPeople.reduce((acc, person) => (
+          [{
+            id: person._id,
+            fullName: [person.firstName, person.lastName].join(' '),
+            company: person.companyName,
+            title: person.title,
+          }, ...acc]
+        ), prevOptions)
+      ));
+    }
+  }, [people.loading]);
+
   return (
     <div className="mx-auto text-center max-w-md">
       <Divider className="mt-2 mb-2" />
-      {newPerson.loading ? 'loading' : newPerson.person.firstName}
       <Autocomplete
         className="mt-4 mb-2"
         disablePortal
         id="combo-box"
         options={options}
-        renderInput={(params) => <TextField {...params} label="Select an event" />}
+        renderInput={(params) => <TextField {...params} label={communities.loading ? 'loading' : 'Select an event'} />}
       />
       <p className="mt-2 w-full bg-slate-300 rounded p-1">People in this event right now: {peopleInEvent}</p>
       <p className="mt-2 w-full bg-slate-300 rounded p-1">People not checked in: {peopleNotCheckedIn}</p>
